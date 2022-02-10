9bf7a66836db759d556c3edf9296f3df10cddc0a
Paul Reece <paulreece2@gmail.com>
2022-02-10 11:36:02 -0500

    step 5 achieved

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Add validation!");
+let total = document.getElementById("total");
+let day = document.getElementById("days");
+// let days = day.value;
+let submit = document.getElementById("submit-button");
+let parking = days + "*5";
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  let totaled = day.value * 5;
+  console.log(totaled);
+  total.innerHTML = totaled;
+});
+
+// function validateForm()
