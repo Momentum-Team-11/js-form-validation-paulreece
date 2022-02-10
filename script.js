03aa286a7eac80be9f985dadb3b7497eaad967eb
Paul Reece <paulreece2@gmail.com>
2022-02-10 16:16:10 -0500

    Step 6 achieved, numbers that aren't credit cards and that are less than 16 digits will return an error message

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-console.log("Add validation!");
 let total = document.getElementById("total");
 let day = document.getElementById("days");
-// let days = day.value;
 let submit = document.getElementById("submit-button");
 let parking = days + "*5";
 let date = document.getElementById("start-date");
 let startD = document.getElementById("start-date");
+
+function validateCardNumber(number) {
+  var regex = new RegExp("^[0-9]{16}$");
+  if (!regex.test(number)) return false;
+
+  return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+  var sum = 0;
+  for (var i = 0; i < val.length; i++) {
+    var intVal = parseInt(val.substr(i, 1));
+    if (i % 2 == 0) {
+      intVal *= 2;
+      if (intVal > 9) {
+        intVal = 1 + (intVal % 10);
+      }
+    }
+    sum += intVal;
+  }
+  return sum % 10 == 0;
+}
 
 submit.addEventListener("click", function (e) {
   let d = new Date(startD.value);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let weekDay = ["Mon", "Tue", "Wed", "Thu"];
   let dayName = days[d.getDay(startD)];
+  let cnum = document.getElementById("credit-card").value;
   console.log(dayName);
   if (weekDay.includes(dayName)) {
     let totaled = day.value * 5;
@@ -20,6 +41,7 @@ submit.addEventListener("click", function (e) {
     let totaley = day.value * 7;
     total.innerHTML = "Total: $" + totaley + ".00";
   }
+  validateCardNumber(cnum);
 });
 
 // function validateForm()
